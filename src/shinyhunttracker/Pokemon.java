12103601d8548172fa8846bdd5d564e339b8f54d
Ben Adams <adamsb32578@gmail.com>
2020-06-24 20:20:10 -0500

    Checks if pokemon is breedable

--- src/shinyhunttracker/Pokemon.java
+++ src/shinyhunttracker/Pokemon.java
@@ -1,10 +1,14 @@
 package shinyhunttracker;
 
+import javax.sql.rowset.JoinRowSet;
+
 public class Pokemon {
     String name;
     int generation;
     Boolean breedable;
     Boolean huntable;
+
+    String[] Legendaries = {"Articuno", "Zapdos", "Moltres", "Mewtwo", "Raikou", "Entei", "Suicune", "Lugia", "Ho-Oh", "Regirock", "Regice", "Registeel", "Latias", "Latios", "Kyogre", "Groudon", "Rayquaza", "Uxie", "Mespirit", "Azelf", "Dialga", "Palkia", "Heatran", "Regigigas", "Giratina", "Cresselia", "Cobalion", "Terrakion", "Virizion", "Tornadus", "Thundurus", "Reshiram", "Zekrom", "Landorus", "Kyurem", "Xerneas", "Yveltal", "Zygarde", "Type: Null", "Silvally","Tapu Koko","Tapu Lele","Tapu Bulu","Tapu Fini","Cosmog","Cosmoem","Solgaleo","Lunala","Necrozma","Zacian","Zamazenta","Eternatus","Kubfu","Urshifu","Nihilego","Buzzwole","Pheromosa","Zurkitree","Celesteela","Kartana","Guzzlord","Poipole","Naganadel","Stakataka","Blacephalon","Mew","Celebi","Jirachi","Deoxys","Manaphy","Darkrai","Shaymin","Arceus","Victini","Keldeo","Meloetta","Genesect", "Diancie", "Hoppa", "Volcanion", "Magearna", "Marshadow", "Zeraora", "Meltan", "Melmetal", "Zarude"};
 
     Pokemon(){
         name = "";
@@ -22,7 +26,6 @@ public class Pokemon {
 
 
     public void isLegendary(){
-        String[] Legendaries = {"Articuno", "Zapdos", "Moltres", "Mewtwo", "Raikou", "Entei", "Suicune", "Lugia", "Ho-Oh", "Regirock", "Regice", "Registeel", "Latias", "Latios", "Kyogre", "Groudon", "Rayquaza", "Uxie", "Mespirit", "Azelf", "Dialga", "Palkia", "Heatran", "Regigigas", "Giratina", "Cresselia", "Cobalion", "Terrakion", "Virizion", "Tornadus", "Thundurus", "Reshiram", "Zekrom", "Landorus", "Kyurem", "Xerneas", "Yveltal", "Zygarde", "Type: Null", "Silvally","Tapu Koko","Tapu Lele","Tapu Bulu","Tapu Fini","Cosmog","Cosmoem","Solgaleo","Lunala","Necrozma","Zacian","Zamazenta","Eternatus","Kubfu","Urshifu","Nihilego","Buzzwole","Pheromosa","Zurkitree","Celesteela","Kartana","Guzzlord","Poipole","Naganadel","Stakataka","Blacephalon","Mew","Celebi","Jirachi","Deoxys","Manaphy","Darkrai","Shaymin","Arceus","Victini","Keldeo","Meloetta","Genesect", "Diancie", "Hoppa", "Volcanion", "Magearna", "Marshadow", "Zeraora", "Meltan", "Melmetal", "Zarude"};
         for(String i: Legendaries)
             if(i.compareTo(name) == 0) {
                 huntable = false;
@@ -56,6 +59,13 @@ public class Pokemon {
     }
 
     public Boolean isBreedable(){
+        String[] BreedablePokemon = {"Nidorina", "Nidoqueen", "Pichu", "Cleffa", "Igglybuff", "Togepi", "Unown", "Tyrogue", "Smoochum", "Elekid", "Magby", "Azurill", "Wynaut", "Budew", "Chingling", "Bonsly", "Mime Jr.", "Happiny", "Munchlax", "Riolu", "Mantyke", "Toxel", "Dracozolt", "Arctozolt", "Dracovish", "Arctovish"};
+        for(String i: Legendaries)
+            if(i.compareTo(name) == 0)
+                return false;
+        for(String i: BreedablePokemon)
+            if(i.compareTo(name) == 0)
+                return false;
         return true;
     }
 
