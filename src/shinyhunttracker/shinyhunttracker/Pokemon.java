abdf7d8b09142384718822d58c2ac375556d1652
Ben Adams <adamsb32578@gmail.com>
2022-10-22 00:37:40 -0400

    commented and cleaned up Pokemon

--- src/shinyhunttracker/shinyhunttracker/Pokemon.java
+++ src/shinyhunttracker/shinyhunttracker/Pokemon.java
@@ -4,11 +4,7 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.Objects;
 import java.util.Vector;
 
@@ -18,8 +14,6 @@ public class Pokemon{
     int id; //dex number - 1
     int evoStage; //evolution stage of pokemon
     Boolean breedable; //if the pokemon can breed
-    Boolean huntable = true; //if the pokemon is huntable
-    Boolean legendary; //if the pokemon is legendary
     int form; //current form of the pokemon
     Vector<Vector<Integer>> family = new Vector<>(); //all pokemon in evolution line
     Vector<Integer> regionalForms = new Vector<>(); //all possible regional forms
@@ -27,20 +21,25 @@ public class Pokemon{
     int[] base; //Stores Speed, HP, Special, Attack, Defense
 
     /**
-     * Parses pokemon.json and initializes variables with data from given index
-     * @param id index of the given pokemon
+     * calls the main constructor with id and passes JSONObject from that index
+     * @param id index
      */
     Pokemon(int id){
         this(Objects.requireNonNull(SaveData.readJSON("GameData/pokemon.json", id)), id);
     }
 
+    /**
+     * Loads data from JSONObject
+     * @param pokemonObject data
+     * @param id index
+     */
     Pokemon(JSONObject pokemonObject, int id){
         name.setValue((String) pokemonObject.get("name"));
         generation = (int) (long)  pokemonObject.get("generation");
         breedable = (Boolean) pokemonObject.get("breedable");
-        legendary = (Boolean) pokemonObject.get("legendary");
         this.id = id;
 
+        //Families are 1D unless they branch, then they are 2D
         JSONArray tempJSONArr = (JSONArray) pokemonObject.get("family");
         if(tempJSONArr != null) {
             family.add(new Vector<>());
@@ -77,28 +76,6 @@ public class Pokemon{
         }
     }
 
-    /**
-     * Finds name with given dex number
-     * @param dexNum Dex number
-     * @return Pokemon name
-     */
-    public static String findName(int dexNum){
-        JSONParser jsonParser = new JSONParser();
-
-        try (FileReader reader = new FileReader("GameData/pokemon.json")){
-            //Read JSON file
-            Object obj = jsonParser.parse(reader);
-            JSONArray pokemonList = (JSONArray) obj;
-
-            //parse pokemon data
-            JSONObject pokemonObject = (JSONObject) pokemonList.get(dexNum - 1);
-            return pokemonObject.get("name").toString();
-        }catch (IOException | ParseException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
     @Override
     public String toString(){ return name.getValue(); }
 
@@ -112,12 +89,9 @@ public class Pokemon{
     public Vector<String> getForms(){ return forms; }
     public String getForm(){ return forms.get(form);}
     public String getName(){ return name.getValue(); }
-    public StringProperty getNameProperty(){ return name; }
 
     public Boolean getBreedable(){ return breedable; }
-    public Boolean getHuntable(){ return huntable; }
 
     public void setForm(int form){ this.form = form; }
-    public void setHuntable(Boolean huntable){ this.huntable = huntable; }
     public void setName(String name){ this.name.setValue(name); }
 }
